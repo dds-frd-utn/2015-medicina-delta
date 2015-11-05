4b9bbf23f1beed7ec609614c242deb5bd35a2dff
Juan Bono <juanbono94@gmail.com>
2015-11-05 19:51:06 -0300

    Agregado: * menu de administrador * index
    Cambios:
    * Limpieza de codigo que no se utilizaba
    * Limpieza de plugins que no se utilizaban
    * Limpieza de rutas que no se utilizaban

--- app/actors/JsonManager.scala
+++ app/actors/JsonManager.scala
@@ -5,17 +5,13 @@ import actors.JsonManager.Lista
 import akka.actor.{Props, Actor, ActorRef}
 import play.api.libs.json.{Json, JsValue}
 
-
-
-
 class JsonManager(outChannel: ActorRef, dBActor: ActorRef) extends Actor {
 
   def receive = {
     case json: String => dBActor ! Agregar(json)
-    case Lista(json: JsValue) => {
+    case Lista(json: JsValue) =>
       println("JSONManager: Recibi json y se lo envio al canal de salida!")
       outChannel ! json
-    }
   }
 }
 
@@ -23,4 +19,5 @@ object JsonManager {
   def props(outChannel: ActorRef, dBActor: ActorRef) = Props(classOf[JsonManager], outChannel, dBActor)
 
   case class Lista(json: JsValue)
+
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,35 +1,18 @@
 package controllers
 
-import actors.HelloActor.SayHello
-import models.Paciente
-import play.api._
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.JsValue
 import play.api.mvc._
 import akka.actor._
 import javax.inject._
 import actors.DBActor
-import scala.concurrent.duration._
-import akka.pattern.ask
-import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits._
 import actors._
 import play.api.Play.current
 
 @Singleton
 class Application @Inject()(system: ActorSystem) extends Controller {
-  implicit val timeout: akka.util.Timeout = 5.seconds
-  val helloActor = system.actorOf(HelloActor.props, "hello-actor")
 
   def index = Action {
     Ok(views.html.index.render())
-  }
-
-
-
-  def sayHello(name: String) = Action.async {
-    (helloActor ? SayHello(name)).mapTo[String].map { message =>
-      Ok(message)
-    }
   }
 
   def mostrarContador = Action {
@@ -37,8 +20,6 @@ class Application @Inject()(system: ActorSystem) extends Controller {
   }
 
   val dbActor = system.actorOf(DBActor.props, "db-actor")
-
-  def reverser = WebSocket.acceptWithActor[String,String] { request => out => Reverser.props(out) }
 
   def sendJSON = WebSocket.acceptWithActor[JsValue, String] { req => out => WebSocketChannel.props(out)}
 
